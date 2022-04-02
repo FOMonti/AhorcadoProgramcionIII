c5bda663f8f3994d86f4be630255f5a3aea2f408
FOMonti <montorofernando4315@gmail.com>
2022-04-02 18:15:08 -0300

    Metodos: crearJuego y cargarPalabras

--- src/programa/servicio/JuegoService.java
+++ src/programa/servicio/JuegoService.java
@@ -7,4 +7,28 @@ import programa.entidades.Juego;
 
 public class JuegoService {
 
+	public Juego crearJuego() {
+
+		List<String> palabras = cargarPalabras();
+
+		return null;
+	}
+
+	public List<String> cargarPalabras() {
+		List<String> palabras = new ArrayList<String>();
+		palabras.add("Palabra");
+
+		return palabras;
+	}
+
+	public boolean buscarLetraOPalabraEnJuego(Juego juego, String input) {
+		if (juego.getPalabraEnJuego().length() != input.length()) {
+			restarPuntos(juego);
+		}
+		return false;
+	}
+
+	public void restarPuntos(Juego juego) {
+
+	}
 }
